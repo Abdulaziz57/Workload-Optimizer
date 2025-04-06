5b56943b94fc33dafe1b6e64c2a804e99febffa8
Abdulaziz57 <a.mannai@hotmail.com>
2025-04-06 17:49:46 +0300

    device agnostic setup for model loading

--- models/load_model.py
+++ models/load_model.py
@@ -1,8 +1,16 @@
-# models/load_model.py
 import torch
-import torchvision.models as models
+from torchvision import models
+
+def get_device():
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        return torch.device("mps")
+    else:
+        return torch.device("cpu")
 
 def load_model_and_input():
-    model = models.mobilenet_v2(pretrained=True).eval().to("cpu")
-    dummy_input = torch.randn(1, 3, 224, 224).to("cpu")
+    device = get_device()
+    model = models.mobilenet_v2(weights="IMAGENET1K_V1").eval().to(device)
+    dummy_input = torch.randn(1, 3, 224, 224).to(device)
     return model, dummy_input
